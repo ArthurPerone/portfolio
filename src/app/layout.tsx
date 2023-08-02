b27506eeb0a6f463c3597ae08787e49904cbfee0
Arthur Perone Barbosa <arthur.b.perone@outlook.com>
2023-08-02 13:03:13 -0300

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.scss'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Arthur Perone - Portifólio',
+  description: 'Esse é o portifólio do desenvolvedor FrontEnd Jr. Arthur Pe',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
