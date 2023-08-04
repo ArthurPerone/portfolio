e1de87f817acca094b01fb547ac2b48787fab741
Arthur Perone Barbosa <arthur.b.perone@outlook.com>
2023-08-03 23:14:36 -0300

    fix bugs

--- src/app/components/technologies/technologies.tsx
+++ src/app/components/technologies/technologies.tsx
@@ -10,7 +10,7 @@ import { GitIcon } from "../icons/gitIcon"
 import { TypescriptIcon } from "../icons/typescriptIcon"
 
 export function Technologies(){
-    const works = {
+    const techs = {
         'HTML':{
             imgSrc:<HTMLIcon />,
             title:'HTML',
@@ -42,26 +42,28 @@ export function Technologies(){
             description:'Superset ao JavaScript, adiciona tipagem estática e facilita o desenvolvimento escalável e robusto',
         }
     }
-    function work(){
-        let listWorks = []
-        for(const work in works){
-            listWorks.push(
+    function tech(){
+        let listTechs = []
+        for(const tech in techs){
+            listTechs.push(
                 <div className="tech">
-                    <ItemTitle text={works[work].title}></ItemTitle>
-                    {works[work].imgSrc}
-                    <p>{works[work].description}</p>
+                    <ItemTitle text={techs[tech].title}></ItemTitle>
+                    {techs[tech].imgSrc}
+                    <p>{techs[tech].description}</p>
                 </div>
             )
         }
         return(
-            listWorks
+            listTechs
         );
     }
 
     return(
         <div className="technologies">
             <SectionTitle text={"Tecnologias"}/>
-            <div className="techsList" children={work()}></div>
+            <div className="techsList">
+                {tech()}
+            </div>
         </div>
     )
 }
